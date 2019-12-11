df5e5d8e26a3ac41a7c31b001c99d77bd01a72ec
aleksandr_savchuk <aleksandr_savchuk@digitasuits.co>
2019-12-11 18:16:38 +0200

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :users, param: :_username
+      post '/login', to: 'authentication#login'
+      get '/*a', to: 'application#not_found'
+    end
+  end
+end
