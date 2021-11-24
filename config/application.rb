b0cc31f450fc83109b300d84d588eca15c8deb1e
alex_savchuk <aleksandr.savchuk@digitalsuits.co>
2021-11-24 22:33:23 +0200

    Fix

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module ShopApi
       end
     end
 
+    config.assets.initialize_on_precompile = false
+
     config.eager_load_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
